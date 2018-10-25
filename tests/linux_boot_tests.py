1857e3b3651065b8d8332c4be5b15b0746b28d2f
Ioannis Doudalis <ioannis.doudalis@esperantotech.com>
2018-10-25 21:50:51 +0000

    [ARCHSIM-76] Add linux kernel checkpoint riscvemu test.
    Add simple test that uses the golden kernel, drops a checkpoint in
    the middle of the boot, restarts the boot and makes sure that we
    reach all way to the end of the boot.

--- tests/linux_boot_tests.py
+++ tests/linux_boot_tests.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import unittest
-import pexpect
 import pytest
 import os
 import subprocess
@@ -16,3 +15,16 @@ def test_linux_boot(output_dir, vharness):
     print(" ".join(cmd))
     subprocess.check_call(cmd, timeout=600)
 
+
+def test_linux_checkpoint_create(output_dir, vharness):
+    linux_cfg = os.path.join(output_dir, 'bbl-vmlinux0.cfg')
+    chpt_filename = os.path.join(output_dir, 'linux-checkpoint.snap')
+    checkpoint_cmd = [vharness, '--save', chpt_filename,
+                      '--maxinsns', '150000000', linux_cfg]
+    print(" ".join(checkpoint_cmd))
+    subprocess.run(checkpoint_cmd, stdout=sys.stdout, stderr=sys.stderr,
+                   check=True, timeout=600)
+    restore_cmd = [vharness, '--load', chpt_filename, linux_cfg,
+                   '--terminate-event', 'linux-boot']
+    print(" ".join(restore_cmd))
+    subprocess.check_call(restore_cmd, timeout=600)
