88a5ac705d796c8f8232b08e7afe3b8a623ec411
Ioannis Doudalis <ioannis.doudalis@esperantotech.com>
2018-10-25 20:29:52 +0000

    [ARCHSIM-78] Improve the linux boot test
    Improve the linux-boot test to:
    * Use the golden kernel we use for generating the checkpoints
    * Use the stop validation event to stop the simulation

--- tests/linux_boot_tests.py
+++ tests/linux_boot_tests.py
@@ -12,9 +12,7 @@ sys.dont_write_bytecode = True
 
 def test_linux_boot(output_dir, vharness):
     linux_cfg = os.path.join(output_dir, 'bbl-vmlinux0.cfg')
-    cmd = [vharness, linux_cfg]
+    cmd = [vharness, linux_cfg, '--terminate-event', 'linux-boot']
     print(" ".join(cmd))
-    child = pexpect.spawn(" ".join(cmd), encoding='utf-8', cwd=output_dir)
-    child.logfile = sys.stdout
-    child.expect("Kernel panic - not syncing: VFS: Unable to mount root fs on", timeout=60)
-    child.close()
+    subprocess.check_call(cmd, timeout=600)
+
