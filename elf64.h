3ccdfc39bff0c34f50bf1a75524aa43200e594bd
Jose Renau <jose.renau@esperantotech.com>
2018-11-16 13:50:34 -0800

    ubuntu has RM_RISCV in the system includes

--- elf64.h
+++ elf64.h
@@ -4,15 +4,17 @@
  * Copyright (c) 2018 Esperanto Technology
  */
 
+#ifndef _ELF64_H
+#define _ELF64_H 1
+
 #include <elf.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
 
-#ifndef _ELF64_H
-#define _ELF64_H 1
-
+#ifndef EM_RISCV
 #define EM_RISCV          0xF3 /* Little endian RISC-V, 32- and 64-bit */
+#endif
 
 bool elf64_is_riscv64(const char *image, size_t image_size);
 bool elf64_find_global(const char *image, size_t image_size,
