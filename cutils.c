fc22ff29a31d6dd143299a7c42b7a92d362a08b0
Tommy Thorn <tommy.thorn@esperantotech.com>
2018-11-27 23:52:46 +0000

    Copyright

--- cutils.c
+++ cutils.c
@@ -1,6 +1,16 @@
+//******************************************************************************
+// Copyright (C) 2018, Esperanto Technologies Inc.
+// The copyright to the computer program(s) herein is the
+// property of Esperanto Technologies, Inc. All Rights Reserved.
+// The program(s) may be used and/or copied only with
+// the written permission of Esperanto Technologies and
+// in accordance with the terms and conditions stipulated in the
+// agreement/contract under which the program(s) have been supplied.
+//------------------------------------------------------------------------------
+
 /*
  * Misc C utilities
- * 
+ *
  * Copyright (c) 2016-2017 Fabrice Bellard
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
