c27cbf5657f149ef1410f7401d08882d7cd3c4e8
Ioannis Doudalis <ioannis.doudalis@esperantotech.com>
2018-11-30 00:31:05 +0000

    [copyright] Fix python scripts after copyright was added

--- tests/chpt_test_riscv_tests.py
+++ tests/chpt_test_riscv_tests.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #******************************************************************************
 # Copyright (C) 2018, Esperanto Technologies Inc.
 # The copyright to the computer program(s) herein is the
@@ -7,8 +9,6 @@
 # in accordance with the terms and conditions stipulated in the
 # agreement/contract under which the program(s) have been supplied.
 #------------------------------------------------------------------------------
-
-#!/usr/bin/env python3
 
 import unittest
 import pexpect
